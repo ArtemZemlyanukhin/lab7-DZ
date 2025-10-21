7e36f4d9178e724a0f0ec2fb61791e17db7469e1
User <User@WIN-RPN3CS32D63>
2025-10-21 17:58:45 +0300

    Добавьте файлы проекта.

---
+++ lab7 DZ/TaskLAB7.c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <locale.h>
+task();
+main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	task();
+}
+ task()
+{
+		int k, jan1, day_year;
+		printf("Введите номер дня k\n");
+		scanf("%d", &k);
+		printf("Введите день недели 1 января\n");
+		scanf("%d", &jan1);
+		day_year = (jan1 + k - 1) % 7;
+		switch (day_year)
+		{
+		case 0:
+			printf("%d день в году-воскресенье\n", k);
+			break;
+		case 1:
+			printf("%d день в году-понедельник\n", k);
+			break;
+		case 2:
+			printf("%d день в году-вторник\n", k);
+			break;
+		case 3:
+			printf("%d день в году-среда\n", k);
+			break;
+		case 4:
+			printf("%d день в году-четверг\n", k);
+			break;
+		case 5:
+			printf("%d день в году-пятница\n", k);
+			break;
+		case 6:
+			printf("%d день в году-суббота\n", k);
+			break;
+
+		}
+	
+}
+
