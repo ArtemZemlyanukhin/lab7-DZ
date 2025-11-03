c70db64e75c23fc5a891d9897e4cda95d342a5c8
Артём Землянухин <zemluanukhin.07@gmail.com>
2025-11-03 18:36:01 +0300

    Add files via upload

---
+++ lab7 DZ/DZ lab7 NEW.c
@@ -0,0 +1,46 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define _CRT_SECURE_NO_DEPRECATE 
+void task();
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	task();
+}
+void task()
+{
+	int k, jan1, day_year;
+	char* day="";
+	printf("Введите номер дня k\n");
+	scanf("%d", &k);
+	printf("Введите день недели 1 января\n");
+	scanf("%d", &jan1);
+	day_year = (jan1 + k - 1) % 7;
+	switch (day_year)
+	{
+	case 0:
+		day = "Воскресенье";
+		break;
+	case 1:
+		day = "Понедельник";
+		break;
+	case 2:
+		day = "Вторник";
+		break;
+	case 3:
+		day = "Среда";
+		break;
+	case 4:
+		day = "Четверг";
+		break;
+	case 5:
+		day = "Пятница";
+		break;
+	case 6:
+		day = "Суббота";
+		break;
+
+	}
+	printf("%d день в году - %s\n", k, day);
+
+}
